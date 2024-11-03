ad1a3eb47d088055879d6eddd81045d9cfc7441f
Oluwadamilare E <joshezekiel.dev@gmail.com>
2024-11-03 20:14:24 +0100

    Add files via upload

---
+++ fund-d-future/src/main/java/com/funddfuture/fund_d_future/FundDFutureApplication.java
@@ -0,0 +1,13 @@
+package com.funddfuture.fund_d_future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundDFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundDFutureApplication.class, args);
+	}
+
+}
---
+++ fund-d-future/src/test/java/com/funddfuture/fund_d_future/FundDFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funddfuture.fund_d_future;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundDFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
