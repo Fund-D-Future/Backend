20b462081f4a946e30ec4e1e4804e983dbc03fb5
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-11-07 14:23:43 +0100

    feat: Implement security feature in repo to avoid sql injection in campaign

--- fund-d-future/src/main/java/com/funddfuture/fund_d_future/campaign/CampaignRepository.java
+++ fund-d-future/src/main/java/com/funddfuture/fund_d_future/campaign/CampaignRepository.java
@@ -1,10 +1,13 @@
 package com.funddfuture.fund_d_future.campaign;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface CampaignRepository extends JpaRepository<Campaign, UUID> {
-    List<Campaign> findByOwnerId(UUID ownerId);
+    @Query("SELECT c FROM Campaign c WHERE c.ownerId = :ownerId")
+    List<Campaign> findByOwnerId(@Param("ownerId") UUID ownerId);
 }
