2f82f922ec899598ad61af2825903924739a912c
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-11-16 17:57:11 +0100

    fix: add component scan

--- src/main/java/com/funddfuture/fund_d_future/FundDFutureApplication.java
+++ src/main/java/com/funddfuture/fund_d_future/FundDFutureApplication.java
@@ -1,6 +1,7 @@
 package com.funddfuture.fund_d_future;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = {"com.funddfuture.fund_d_future"})
 @RestController
 public class FundDFutureApplication {
 
